f0cbeaf9905a04bd37daa96072b2c0f979c935c8
Murat Demirtas <73036794+muratdemirtas2001@users.noreply.github.com>
2020-12-31 15:12:46 +0000

    adding second way to toggle

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-//using selectors inside the element
-// traversing the dom
-
-const btns=document.querySelectorAll('.question-btn');
-btns.forEach((btn)=>{
-    btn.addEventListener('click',(e)=>{
+// we have 2 option 1-traversing the dom 2-using selectors inside the element
+// 1-traversing the dom
+// const btns=document.querySelectorAll('.question-btn');
+// btns.forEach((btn)=>{
+//     btn.addEventListener('click',(e)=>{
         
-        // const question=e.path[4]; you can also reach the element with path
+//         // const question=e.path[4]; you can also reach the element with path
 
-        const question= e.currentTarget.parentElement.parentElement;
+//         const question= e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//     })
+// })
+
+
+//2-using selectors inside the element
+
+const questions=document.querySelectorAll('.question');
+// console.log(questions);
+questions.forEach((question)=>{
+    // console.log(question);
+    btn= question.querySelector('.question-btn');
+    btn.addEventListener('click',()=>{
         question.classList.toggle('show-text');
     })
+
+
 })
