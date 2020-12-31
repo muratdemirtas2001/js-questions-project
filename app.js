8a9e15f98902f3b7bc9c9fc6e2f6f496a92b372e
Murat Demirtas <73036794+muratdemirtas2001@users.noreply.github.com>
2020-12-31 14:52:37 +0000

    adding event listener to buttons and  toggling classList

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 //using selectors inside the element
 // traversing the dom
+
+const btns=document.querySelectorAll('.question-btn');
+btns.forEach((btn)=>{
+    btn.addEventListener('click',(e)=>{
+        
+        // const question=e.path[4]; you can also reach the element with path
+
+        const question= e.currentTarget.parentElement.parentElement;
+        question.classList.toggle('show-text');
+    })
+})
