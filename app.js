2c3a6b29061410f348176bc910f7de2e83ed2471
Murat Demirtas <73036794+muratdemirtas2001@users.noreply.github.com>
2020-12-31 15:37:37 +0000

    finishing project with two option to use in JS

--- app.js
+++ app.js
@@ -1,17 +1,4 @@
 // we have 2 option 1-traversing the dom 2-using selectors inside the element
-// 1-traversing the dom
-// const btns=document.querySelectorAll('.question-btn');
-// btns.forEach((btn)=>{
-//     btn.addEventListener('click',(e)=>{
-        
-//         // const question=e.path[4]; you can also reach the element with path
-
-//         const question= e.currentTarget.parentElement.parentElement;
-//         question.classList.toggle('show-text');
-//     })
-// })
-
-
 //2-using selectors inside the element
 
 const questions=document.querySelectorAll('.question');
@@ -20,8 +7,30 @@ questions.forEach((question)=>{
     // console.log(question);
     btn= question.querySelector('.question-btn');
     btn.addEventListener('click',()=>{
+        questions.forEach((item)=>{
+            if(question !== item){
+                item.classList.remove('show-text');
+            }
+        })
+
         question.classList.toggle('show-text');
     })
 
 
 })
+
+
+// 1-traversing the dom
+// const btns=document.querySelectorAll('.question-btn');
+// btns.forEach((btn)=>{
+//     btn.addEventListener('click',(e)=>{
+        
+//         // const question=e.path[4]; you can also reach the element with path
+
+//         const question= e.currentTarget.parentElement.parentElement;
+          
+//         question.classList.toggle('show-text');
+//     })
+// })
+
+
